0fcc8f30e3104e8d452849ed72445888d87a542f
unknown <matchulaod@gmail.com>
2023-07-18 20:16:05 -0300

    Update machine flags and update the autostart settings

--- Code/compile.sh
+++ Code/compile.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-make clean -C /home/infralab/backupCodigoTCC2/Code 
-make -C /home/infralab/backupCodigoTCC2/Code
-make -C /home/infralab/backupCodigoTCC2/Code run -j 2
+sudo /home/infralab/backupCodigoTCC2/Code/execute_machine
---
+++ Code/interface.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo rm /home/infralab/interface 
+sudo rm /home/infralab/machine
+
+sudo /home/infralab/backupCodigoTCC2/Code/build/Interface
+
+
+
+
